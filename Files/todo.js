172f739ca90fb04fdf3996a7d7ac92ceacc25b65
sweety <ganesh@example.com>
2026-06-12 17:28:12 +0530

    performed save/remove/add operations on files

--- Files/todo.js
+++ Files/todo.js
@@ -1,12 +1,46 @@
 const fs=require('fs');
-const path="./tasks.json";
+const filepath="./tasks.json";
+
+const command=process.argv[2];
+const argument=process.argv[3];
+
+// writing arrow function
+const loaddata=()=>{
+    try{
+        const databuffer=fs.readFileSync(filepath)
+        const data=databuffer.toString()
+        return JSON.parse(data);
+    }catch(error){
+        return []
+    }
+}
+const savetasks=(tasks)=>{
+    const datajson=JSON.stringify(tasks)
+    fs.writeFileSync(filepath,datajson);
+}
+const addtask =(task)=>{
+    const tasks=loaddata()
+    tasks.push({task});
+    savetasks(tasks);
+    console.log("task added successfully",task);
+}
+const listtasks=()=>{
+    const list=loaddata();
+    list.forEach((item,index)=>console.log(`${index+1} - ${item.task}`));
+}
+const removetask=(idx)=>{
+    const list=loaddata();
+    const original_idx=idx-1;
+    list.splice(original_idx,1);
+    savetasks(list);
+}
 
 if(command==="add"){
-    addtask(task);
-}else if(command=="list"){
+    addtask(argument);
+}else if(command==="list"){
     listtasks();
 }else if(command==="remove"){
-    removetask(parseInt(taskno));
+    removetask(parseInt(argument));
 }else{
     console.log("command not found");
 }
