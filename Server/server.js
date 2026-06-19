f162fd9b735a74b4f71febedc1cabd481fd0f1c8
sweety <ganesh@example.com>
2026-06-19 23:47:26 +0530

    created server and made http request

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,32 @@
 const http=require('http');
 const fs=require('fs');
 const path=require('path');
+const port=3000;
+
+const server=http.createServer((req,res)=>{
+    const filepath=path.join(__dirname,req.url==='/' ? "index.html": req.url)
+
+    const extName=String(path.extname(filepath).toLowerCase())
+    const mimeTypes={
+        '.html':'text/html',
+        '.css':'text/css',
+        '.js':'text/javascript',
+        '.png':'png/img',
+    }
+    const contenttype=mimeTypes[extName]||'application/octet-stream'
+    fs.readFile(filepath,(err,content)=>{
+        if(err){
+            if(err.code==="ENOENT"){
+                res.writeHead(404,{"content-Type":"text/html"});
+                res.end("404: File not found BRuhhh");
+            }
+        }else{
+            res.writeHead(200,{'content-type':contenttype});
+            res.end(content,'utf-8');
+        }
+    })
+
+});
+server.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+})
